43b639d2150989065bc456daf47b60bc345234a1
Amir <amir.ukic123@gmail.com>
2023-01-23 23:14:21 +0100

    commit

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 function Home() {
   return (
     <div className="scssClass">
-      <h2>React Js SASS / SCSS Examples</h2>
+      <h2>React Js SASS / SCSS Example</h2>
     </div>
   )
 }
