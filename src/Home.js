5eaecdbdfa442f4b9d79b685448651e624c2cb89
Amir <amir.ukic123@gmail.com>
2023-01-26 00:50:23 +0100

    partials

--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-function Home() {
-  return (
-    <div className="scssClass">
-      <h2>React Js SASS / SCSS Example</h2>
-    </div>
-  )
-}
-export default Home
---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Home() {
+  return (
+    <div className="main">
+      <p className="main_paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+        quos maxime pariatur vel, deserunt eius quam nobis unde distinctio odio
+        fuga? Quisquam fuga iusto similique, explicabo repellendus harum quasi
+        optio?
+      </p>
+      <p className="main_paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut deserunt
+        ipsa quaerat alias sit officia quam eos, quos, in quibusdam, soluta
+        ullam? Dolor tempore ea repellendus? Qui quos temporibus quibusdam.
+      </p>
+    </div>
+  );
+}
+export default Home;
