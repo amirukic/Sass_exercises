7d85d51800ff43e43fc905f5174ceda746c1db76
Amir <amir.ukic123@gmail.com>
2023-01-31 19:07:11 +0100

    extend_sass

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 function Home() {
   return (
     <div className="main">
-      <p className="main_paragraph">
+      <p className="main_paragraph1">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
         quos maxime pariatur vel, deserunt eius quam nobis unde distinctio odio
         fuga? Quisquam fuga iusto similique, explicabo repellendus harum quasi
         optio?
       </p>
-      <p className="main_paragraph">
+      <p className="main_paragraph2">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut deserunt
         ipsa quaerat alias sit officia quam eos, quos, in quibusdam, soluta
         ullam? Dolor tempore ea repellendus? Qui quos temporibus quibusdam.
